49daff4dd79198fca68f29bb89185dfab8da4612
Anas Elmi <elmi.anas@gmail.com>
2018-02-19 13:33:32 -0800

    weird shit fam

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+import pygame
+from pygame.locals import *
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -10,3 +12,9 @@ PURPLE = (128, 0, 128)
 PINK = (255, 200, 200)
 BROWN = (139, 69, 19)
 GOLD = (255, 255, 0)
+
+# Default Button Mapping
+BUTTON_MAP = {
+    K_ESCAPE: "QUIT",  # Escape
+    K_UP: "MOVE"
+}
--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import sys
 import pygame
 from pygame.locals import *
 from consts import *
-
+from event_handler import *
 # Initializing everything
 pygame.init()
-pygame.display.set_mode((0,0))
+pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption("Puzzler: The Puzzle")
-
 FINALSURFACE = pygame.Surface((0,0))
+EventHandler = EventHandler()
 
+# Main game loop
 while True:
-    pygame.quit()
-    sys.exit()
+
+    for event in pygame.event.get():
+        EventHandler.parse_event(event)
+
+    pygame.display.update()
