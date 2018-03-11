85e5fdc0ad821cc603f79fa49e8a96ef61dc0ee5
Anas Elmi <elmi.anas@gmail.com>
2018-03-10 21:54:07 -0800

    omg there is actually text on the screen btw i wasted so much creating skeleton classes i havent used yet and swetting small stuff sorry

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ import tcod
 from consts import *
 from handle_input import *
 from unit import *
+from message_console import *
 
 
 def main():
@@ -12,20 +13,23 @@ def main():
     running = True
     while not tcod.console_is_window_closed() and running:
 
-        tcod.console_set_default_foreground(0, tcod.white)
         Player.draw(con)
         tcod.console_blit(con, 0, 0, SCREEN_X + 1, SCREEN_Y, 0, 0, 0)
         tcod.console_flush()
         Player.clear(con)
 
+        MesCon.draw("WOW")
+        MesCon.blit(con)
+        MesCon.clear()
+
         tcod.sys_wait_for_event(tcod.EVENT_KEY_PRESS, key, mouse, True)
         action = handle_keys(key)
         move = action.get("move")
-        exit = action.get("exit")
+        escape = action.get("exit")
 
         if move:
             Player.move(move)
-        if exit:
+        if escape:
             running = False
         # TODO: use handle_keys and dictionaries to parse and perform actions
 
@@ -35,6 +39,9 @@ if __name__ == "__main__":
     tcod.console_init_root(SCREEN_X, SCREEN_Y, GAME_TITLE)
 
     con = tcod.console_new(SCREEN_X, SCREEN_Y)
+    tcod.console_set_default_foreground(0, tcod.white)
+
+    MesCon = MessageConsole(SCREEN_X, SCREEN_Y)
 
     Player = Unit(CENTER_X, CENTER_Y, T_PLAYER)
 
--- message_console.py
+++ message_console.py
@@ -1,12 +1,15 @@
 import tcod
 import itertools
 
+
 class MessageConsole:
     def __init__(self, screen_x, screen_y):
-        self.x = screen_x / 6
-        self.y = screen_y / 3
+        self.x = int(screen_x / 6)
+        self.y = int(screen_y / 3)
         self.console = tcod.console_new(self.x, self.y)
         self.message_archive = []
+
+        tcod.console_set_alignment(self.console, tcod.LEFT)
 
     def update_archive(self, messages):
         if isinstance(messages, str):
@@ -14,6 +17,21 @@ class MessageConsole:
 
         elif isinstance(messages, list):
             self.message_archive = itertools.chain(self.message_archive, messages)
+
+        if len(self.message_archive) >= 100:
+            pass
+
+    def draw(self, text):
+        tcod.console_print(self.console, 0, 0, text)
+
+    def clear(self):
+        self.console.clear()
+
+    def blit(self, console):
+        tcod.console_blit(self.console, 0, 0, 0, 0, console, 0, 0)
+
+
+
 
 
 
