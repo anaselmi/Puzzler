25a8dc73a5ce8aac4863e3e903ffba95562662c8
Anas Elmi <elmi.anas@gmail.com>
2018-04-05 11:25:36 -0700

    commining and pushing because im just going to try someone elses library and see if that solves the problems ive ran into

--- src/ecs/ecs_components.py
+++ src/ecs/ecs_components.py
@@ -1,59 +1,12 @@
-import tdl
-from ecs_meta import Component
-from src.consts import *
-
-render_allowed_events = ["draw", "clear", "scroll"]
+from ecs_manager import *
 
 
-class RenderComponent(Component):
+class Renderable(Component):
 
-    def __init__(self, **kwargs):
-        priority = 0
-        super().__init__(priority, **kwargs)
+    def __init__(self, tags):
+        valid_actions = ["draw", "clear"]
+        super().__init__(self, valid_actions, tags)
 
     def parse(self, event):
-        if not self.active:
-            return None
-
-        if event.type == "draw":
-            self.tag_ensure(x=None, y=None, char="@", fg=WHITE, bg=BLACK)
-            self.owner.manager.window.draw_char(self.x, self.y, self.char, self.x, self.y, self.char, bg=(0, 0, 0), fg=(255, 255, 255))
-
-        elif event.type == "clear":
-            pass
-
-        elif event.type == "scroll":
-            pass
-
-        else:
-            pass
-
-move_allowed_events = ["move"]
-
-
-# TODO
-class MoveComponent(Component):
-
-    def __init__(self, **kwargs):
-        priority = 1
-        super().__init__(priority, **kwargs)
-
-    def parse(self, event):
-        if not self.active:
-            return None
-
-        if event.type == "move":
-            pass
-
-
-        else:
-            pass
-
-    def parse_draw(self, event):
-        self.window.draw_char()
-
-    def parse_clear(self, event):
         pass
 
-    def parse_scroll(self, event):
-        pass
--- src/ecs/ecs_manager.py
+++ src/ecs/ecs_manager.py
@@ -1,176 +1,103 @@
 import uuid
-from abc import *
-from itertools import chain
 
 
 # Entities store components, and pass events to components
+# Idea: action order is a dictionary where the key is the action and the value is the sorted list of components
 class Entity:
+    class_id = "EN_"
 
-    def __init__(self, manager, *args):
+    def __init__(self, manager, components, name=None):
+        if name is None:
+            name = str(uuid.uuid4())
+        self.id = Entity.class_id + name
         self.manager = manager
-        self.components = []
-        self.id = "EN_" + str(uuid.uuid4())
-        for component in args:
-            # Makes sure only components are accepted into the components list.
-            if isinstance(component, Component):
-                self.components.append(component)
+        self.components = components
         self.register_components()
 
     def add_component(self, component):
-        if component not in self.components and isinstance(component, Component):
-            component.owner = self.id
-            self.components += component
+        self.components.append(component)
 
     def remove_component(self, component):
-        if component in self.components and isinstance(component, Component):
-            self.components.remove(component)
+        self.components.remove(component)
 
     def register_components(self):
         for component in self.components:
             component.owner = self.id
 
-    def reset_components(self):
+    # Orders components by which parses event first to ensure logical data flow
+    def prioritize_components(self, action):
+        action_order = []
         for component in self.components:
-            component.done = False
+            priority = component.accepts(action)
+            if priority is not None:
+                action_order.append([component, priority])
+        action_order = [x[0] for x in sorted(action_order, key=lambda x: x[1])]
+        return action_order
 
-    # TODO
     def parse_event(self, event):
-        for component in self.components:
-            if component.accept(event):
-                component.parse(event)
-            else:
-                component.done = True
+        action = None
+        while event.has_actions():
+            if action != event.get_action():
+                action = event.get_action()
+                action_order = self.prioritize_components(action)
+            if not action_order:
+                event.remove_action(self.id, action)
+                continue
+            # Smaller priority number implies higher priority
+            for component in action_order:
+                # If a component returns None, then it has added a new event to the front
+                if component.parse(event) is None:
+                    break
 
-    # TODO
-    def request(self, request_id, event):
-        if request_id == self.id:
-            self.parse_event(event)
-        elif request_id is None:
-            self.manager.parse_request(self.id, event)
+
+class Event:
+    class_id = "EV_"
+
+    def __init__(self, actions, tags):
+        self.id = Event.class_id + str(uuid.uuid4())
+        self.actions = actions
+        self.tags = tags
+
+    @staticmethod
+    def create_action(request_id, action):
+        return [request_id, action]
+
+    def add_action_b(self, request_id, action):
+        new_action = self.create_action(request_id, action)
+        self.actions.append(new_action)
+
+    def add_action_f(self, request_id, action):
+        new_action = self.create_action(request_id, action)
+        self.actions.insert(0, new_action)
+
+    def remove_action(self, request_id, action):
+        if request_id[0:3] == Entity.class_id and request_id != action[0]:
+            return None # TODO LOG ERROR HERE
+        # HOW AM I GOING TO LOG ERRORS???
+        self.actions.remove(action)
+        return ...
+
+    def get_action(self):
+        if self.has_actions():
+            return self.actions[0]
+
+    def has_actions(self):
+        if not self.actions:
+            return False
+        return True
 
 
-# Components hold tags, accepted events, and logic for each event
-# TODO eventually: Ordering components and sorting, subscribing
-class Component(ABC):
+class Component:
+    class_id = "CO_"
 
-    def __init__(self, allowed_events, owner, priority=50, done=False, **kwargs):
-        assert(isinstance(allowed_events, list))
-        allowed_events += ["update"]
-        self.allowed_events = list(chain(allowed_events, allowed_events))
+    def __init(self, valid_actions, tags):
+        self.id = Component.class_id + str(uuid.uuid4())
+        self.valid_actions = valid_actions
+        self.tags = tags
 
-        self.owner = owner
-        self.manager = self.owner.manager
+    def accepts(self, action):
+        # Checks if the action is in the list of valid actions
+        return any([x[0] == action for x in self.valid_actions])
 
-        self.priority = priority
-        self.done = done
-
-        self.id = "CO_" + str(uuid.uuid4())
-
-        self.tags = {"active": True}
-        for tag, value in kwargs.items():
-            self.tags[tag] = value
-
-    # TODO
-    def accept(self, event):
-        event_type = event.type
-        if event_type in self.allowed_events:
-            return True
-        return False
-
-    @abstractmethod
     def parse(self, event):
-        pass
-
-
-class Event(ABC):
-
-    def __init__(self, moments, **kwargs):
-        self.id = "EV_" + str(uuid.uuid4())
-
-        self.moments = moments
-
-        self.tags = {"active": True}
-        for tag, value in kwargs.items():
-            self.tags[tag] = value
-
-    # Use to get a tag if it exists or to get a default parameter if it does not
-    def get_tag(self, tag, default=None):
-        value = self.tags.get(tag)
-        if value:
-            return value
-        return default
-
-    # Use if you want to get a tag that should be there
-    def grab_tag(self, tag):
-        value = self.tags.get(tag)
-        if value is None:
-            raise KeyError("expected tag to have a value, but there was none" + tag)
-
-    # Use if you are not certain a tag has been added and want to add a default value
-    def update_tag(self, tag, value):
-        self.tags.update({tag: value})
-
-    # Use if a tag should be there and are fine with raising an error if it hasn't been
-    def change_tag(self, tag, value):
-        if self.get_tag(tag) is None:
-            raise KeyError("expected tag to have a value, but there was none" + tag)
-        self.tags[tag] = value
-
-    # Use if a tag hasn't been added or you want to change a tag that might be there
-    def add_tag(self, tag, value):
-        self.tags[tag] = value
-
-    # Use this to remove a tag, whether or not it exists
-    def remove_tag(self, tag):
-        self.tags.pop(tag, None)
-
-    # TODO: Figure out
-    def add_moment(self, moment, front=True):
-        pass
-
-    def remove_moment(self, moment, front=True):
-        pass
-
-    def current_moment(self):
-        pass
-
-    def add_event_type(self, id, event_type, front=True):
-        pass
-
-    def remove_event_type(self):
-        pass
-
-
-# TODO: EntityManager needs to be able to parse requests, possible entity in ECSM that parses requests
-# TODO: Idea, component saves event, transforms current event into request with needed targets, and resumes original
-class ECSManager:
-    def __init__(self, window):
-        self.window = window
-        self.entities = []
-        self.events = []
-
-        self.id = "EV_" + str(uuid.uuid4())
-
-    # Formats our registry of entities so we have easy access to the entity and its ID
-    # TODO eventually: possibly used a named tuple
-    def register_entity(self, entity):
-        entity_id = entity.id
-        entity_format = (entity_id, entity)
-        self.entities.append(entity_format)
-
-    def dispatch_event(self, event):
-        target_id = event.current_target
-        target = self.id_to_object(target_id)
-        target.parse_event(event)
-
-    # TODO: for example if an entity is requesting whether or not a tile is passable send coordinates to parse_request
-    def parse_request(self, request):
-        pass
-
-    # Allows us to access the object from within IDs without actually referencing the object itself
-    # TODO eventually: possibly used a named tuple
-    def id_to_object(self, entity_id):
-        for entity_format in self.entities:
-            if entity_id == entity_format[0]:
-                return entity_format[1]
-        return None
+        raise NotImplementedError
