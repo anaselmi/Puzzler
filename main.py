14b9cf98f9f138edf497bab519b48fa670c75542
Anas Elmi <elmi.anas@gmail.com>
2018-05-06 15:29:11 -0700

    hi new branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tdl
-import src.ecs.processors as proc
+import src.ecs.processors as pro
 import src.ecs.components as comp
 from src.world_manager import WorldManager
 from src.user_interface.ui_manager import UIManager
@@ -32,15 +32,15 @@ class Engine:
     def create_processors(self):
         processors = []
 
-        message_processor = proc.MessageProcessor(START_MESSAGE)
+        message_processor = pro.MessageProcessor(START_MESSAGE)
         processors.append(message_processor)
-        render_processor = proc.RenderProcessor()
+        render_processor = pro.RenderProcessor()
         processors.append(render_processor)
-        position_processor = proc.VelocityProcessor()
+        position_processor = pro.VelocityProcessor()
         processors.append(position_processor)
-        action_processor = proc.ActionProcessor()
+        action_processor = pro.ActionProcessor()
         processors.append(action_processor)
-        turn_processor = proc.TurnProcessor()
+        turn_processor = pro.TurnProcessor()
         processors.append(turn_processor)
 
         for i, processor in enumerate(processors):
@@ -74,21 +74,22 @@ class Engine:
         self.action_dispatcher(None)
         self.running = True
         while self.running and not tdl.event.is_window_closed():
+            # Updating and drawing to screen
             self.game_ui.update()
             self.root.blit(self.console)
             self.game_ui.clear()
             tdl.flush()
-            # Event handling/logic
+            # Event handling
             _input = tdl.event.key_wait()
             action = InputHandler.process(_input)
             if action == "QUIT_GAME":
                 self.running = False
-            else:
-                self.action_dispatcher(action)
-                self.world.update()
+            self.action_dispatcher(action)
+            self.world.update()
 
 if __name__ == "__main__":
     engine = Engine()
+    # Run these methods in this order or things will crash
     engine.create_action_dispatcher()
     engine.create_game_ui()
     engine.create_world(SCREEN_SIZE)
